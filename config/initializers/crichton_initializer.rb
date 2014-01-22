5c44b54577681b641f34dc9d84b6cffe7312ce8c
brutski <abrutski@mdsol.com>
2014-01-22 11:14:17 -0500

    refactoring. Addressed pr comments

--- config/initializers/crichton_initializer.rb
+++ config/initializers/crichton_initializer.rb
@@ -1,6 +1,3 @@
-require 'bundler/cli'
 require 'crichton'
 
-gem = Bundler.load.specs.find{ |g| g.name == 'crichton' }
-crichton_drd_doc = "#{gem.full_gem_path}/spec/fixtures/resource_descriptors/drds_descriptor_v1.yml"
-Crichton.initialize_registry(crichton_drd_doc)
+Crichton.register_drds_sample
