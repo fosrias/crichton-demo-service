af50f85d206e40f289c10ee777ad425930fdc8f7
brutski <abrutski@mdsol.com>
2014-06-05 16:13:05 -0400

    added crichton_initializer again. calling rake task

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,6 @@ end
 module CrichtonDemoService
   class Application < Rails::Application
 
-    require 'rake'
-    self.load_tasks
-    Rake::Task['load:resource_descriptor'].invoke
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
